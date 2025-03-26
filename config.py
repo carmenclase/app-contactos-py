780e507021ae3552316d7ab9f35ac323b1c7799f
Carmen Clase <183748252+carmenclase@users.noreply.github.com>
2025-03-26 19:36:36 +0000

    dotenv

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
     """Configuración base común a todos los entornos."""
@@ -6,11 +7,9 @@ class Config:
     VERSION = "1.0.0"
 
     # Configuración de la base de datos
-    DB_USER = os.getenv("DB_USER", "contactos_user")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "ChangeMe!")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "3306")
     DB_NAME = os.getenv("DB_NAME", "contactos_db")
+    secret_config = load_dotenv()
 
 def get_config(env=None):
     return Config
