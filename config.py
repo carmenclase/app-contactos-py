e28b7dda350593dff5e66c4b6fa47f34b8bfd626
Ubuntu <ubuntu@ip-172-31-81-120.ec2.internal>
2025-03-26 18:50:36 +0000

    config file

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
 
     # Configuración de la base de datos
     DB_USER = os.getenv("DB_USER", "contactos_user")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "ChangeMe!")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "contactosSQL")
+    DB_HOST = os.getenv("DB_HOST", "carmenclase-mysql.cj0syiy4s7zj.us-east-1.rds.amazonaws.com")
     DB_PORT = os.getenv("DB_PORT", "3306")
     DB_NAME = os.getenv("DB_NAME", "contactos_db")
 
